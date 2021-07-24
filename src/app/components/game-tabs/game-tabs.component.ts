0b3c0bd64bd8c6fab59be6efa1406e4858bb7bbb
kote <kotekirkitadze@gmail.com>
2021-07-24 15:03:22 +0400

    some bugs needs to be fixed

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-tabs',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-tabs.component.scss']
 })
 export class GameTabsComponent implements OnInit {
+  @Input() game: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("my", this.game);
   }
 
 }
